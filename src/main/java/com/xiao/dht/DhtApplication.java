245b4546bea26fd1fcbd66b33fb5052bdeaf87f6
27794 <2779435956@qq.com>
2023-07-24 21:15:16 +0800

    doc[doc]:v1.0

---
+++ src/main/java/com/xiao/dht/DhtApplication.java
@@ -0,0 +1,13 @@
+package com.xiao.dht;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DhtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DhtApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/xiao/hello.java
@@ -0,0 +1,4 @@
+package com.xiao;
+
+public class hello {
+}
